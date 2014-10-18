82b55c02ffe8b1838db0b507e429cb8e5902f073
samarth <bansalsamarth@yahoo.com>
2014-10-18 21:10:58 +0530

    Added templates

--- music/urls.py
+++ music/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import *
 
 urlpatterns = patterns('music.views', 
-    (r'^$', 'main_page')
+    (r'^$', 'main_page'),
+	(r'^browse$', 'browse_page'),
+	(r'^music$', 'my_music'),
+	(r'^featured$', 'featured_page'),
+	(r'^about$', 'about')
 )
--- music/views.py
+++ music/views.py
@@ -8,5 +8,21 @@ from django.contrib.auth.decorators import login_required
 
 @login_required()
 def main_page(request):
-    return render(request, 'index.html', {}, context_instance = RequestContext(request))
+    return render(request, 'index.html', {'active':'home'}, context_instance = RequestContext(request))
     #return HttpResponse("Hello World!")
+
+@login_required()
+def browse_page(request):
+	return render(request, 'browse.html', {'active':'browse'}, context_instance = RequestContext(request))
+
+@login_required()
+def my_music(request):
+	return render(request, 'music.html', {'active':'my_music'}, context_instance = RequestContext(request))
+
+@login_required()
+def featured_page(request):
+	return render(request, 'featured.html', {'active':'featured'}, context_instance = RequestContext(request))
+
+@login_required()
+def about(request):
+	return render(request, 'about.html', {'active':'about'}, context_instance = RequestContext(request))
