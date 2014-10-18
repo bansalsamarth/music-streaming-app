b5000c742cbeb7df5b343e45521b19a6993c53ac
samarth <bansalsamarth@yahoo.com>
2014-10-18 12:50:04 +0530

    Home Page Initial

--- music/views.py
+++ music/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
 
 # Create your views here.
 
 def main_page(request):
-    return HttpResponse("Hello World!")
+    return render(request, 'index.html', {}, context_instance = RequestContext(request))
+    #return HttpResponse("Hello World!")
