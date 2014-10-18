f2a62403869b2bb3e8c2a7ad074428a12f2de27c
samarth <bansalsamarth@yahoo.com>
2014-10-18 14:38:29 +0530

    Login Page Added

--- audio_streaming_app/urls.py
+++ audio_streaming_app/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.contrib.auth.views import login, logout
 import music.urls as music_urls
 
 urlpatterns = patterns('',
@@ -11,5 +12,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include(music_urls)),
+    url(r'^accounts/login/$', login, {'template_name': 'login.html'}),
+    url(r'^accounts/logout/$', logout),
+    url(r'^', include(music_urls)),
 )
--- music/views.py
+++ music/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
+@login_required()
 def main_page(request):
     return render(request, 'index.html', {}, context_instance = RequestContext(request))
     #return HttpResponse("Hello World!")
