5bd4334771d2c807f92eac39b3107a1d97691eb8
samarth <bansalsamarth@yahoo.com>
2014-11-27 03:03:58 +0530

    New URLs added

--- music/urls.py
+++ music/urls.py
@@ -5,5 +5,8 @@ urlpatterns = patterns('music.views',
 	(r'^browse$', 'browse_page'),
 	(r'^music$', 'my_music'),
 	(r'^featured$', 'featured_page'),
-	(r'^about$', 'about')
+	(r'^about$', 'about'),
+	(r'^radio/(?P<username>\w*)$', 'radio_page'),
+	(r'^radio_setup$', 'setup_radio'),
+
 )
